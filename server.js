36f8a274381ca688c7d3604ca975f4f5fb5af23a
Rohit Kumar <98901698+rkctech@users.noreply.github.com>
2024-01-12 11:09:53 +0530

    Update server.js
    final change in code

--- server.js
+++ server.js
@@ -2,49 +2,50 @@ import express from "express";
 import colors from "colors";
 import dotenv from "dotenv";
 import morgan from "morgan";
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
 import connectDB from "./config/db.js";
 import authRoutes from "./routes/authRoute.js";
 import categoryRoutes from "./routes/categoryRoutes.js";
 import productRoutes from "./routes/productRoutes.js";
 import cors from "cors";
-import path from "path"
+import path from "path";
 
-//configure env
+// Configure env
 dotenv.config();
 
-// es mpduel fix
-const __filename =fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-//databse config
+// Database config
 connectDB();
 
-//rest object
+// Rest object
 const app = express();
 
-//middelwares
+// Middlewares
 app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
-app.use(express.static(path.join(__dirname,"./client/build")));
 
-//routes
+// Use static files from client/build
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+app.use(express.static(path.join(__dirname, "./client/build")));
+
+// Routes
 app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/category", categoryRoutes);
 app.use("/api/v1/product", productRoutes);
 
-//rest api
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname,"./client/build/index.html"));
+// Rest API
+app.use("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
 
-//PORT
+// PORT
 const PORT = process.env.PORT || 8080;
 
-//run listen
+// Run server
 app.listen(PORT, () => {
   console.log(
-    `Server Running on ${process.env.DEV_MODE} mode on port ${PORT}`.bgCyan
-      .white
+    `Server Running on ${process.env.DEV_MODE} mode on port ${PORT}`.bgCyan.white
   );
 });
